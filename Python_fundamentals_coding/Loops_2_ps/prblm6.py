dd0543f4ae6fecbe604c42985226a243a21ae510
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-01 07:04:09 +0530

    Update prblm6.py

--- Python_fundamentals_coding/Loops_2_ps/prblm6.py
+++ Python_fundamentals_coding/Loops_2_ps/prblm6.py
@@ -3,3 +3,12 @@ product=1
 for i in range(1,n+1):
     product=product*i
 print(f"The factorial of {n} is {product}")
+# Using while loop
+n=int(input("Enter the number"))
+
+i=1
+res=1
+while(i<=n):
+    res=res*i
+    i+=1
+print(f"The factorial of {n} is {res}")
