b5364ec7bb642bccb9876d359f7ec96a175ce440
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-01 06:23:56 +0530

    done bro

---
+++ Python_fundamentals_coding/Loops_2/06_for_with_else.py
@@ -0,0 +1,9 @@
+for i in range(11):
+    print(i)
+else:
+    print("Its done bro") # this is printed when the loop exhausts\
+n="rabbani"
+for i  in n:
+    print(i)
+else:
+    print("It's done")
---
+++ Python_fundamentals_coding/Loops_2/07_break_continue.py
@@ -0,0 +1,10 @@
+for i in range(100):
+# Using the break stmnt
+    if(i==34):
+        break #exit the loop ryt now
+    print(i)
+# Using the continue stmnt
+for i in range(11):
+    if(i==4):
+        continue #skip this iteration
+    print(i)
