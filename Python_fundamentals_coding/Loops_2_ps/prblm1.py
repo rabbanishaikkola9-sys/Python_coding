556358847e34370409f9a6f4b474c23cd4a4f26b
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-01 06:46:32 +0530

    files added

---
+++ Python_fundamentals_coding/Loops_2_ps/prblm1.py
@@ -0,0 +1,15 @@
+# WAPP to print the multiplication of the given number
+n=int(input("Enter the number"))
+for i in range(1,11):
+    print(f"{n} x {i} = {n * i}")
+# output:
+# 25 x 1 = 25
+# 25 x 2 = 50
+# 25 x 3 = 75
+# 25 x 4 = 100
+# 25 x 5 = 125
+# 25 x 6 = 150
+# 25 x 7 = 175
+# 25 x 8 = 200
+# 25 x 9 = 225
+# 25 x 10 = 250
