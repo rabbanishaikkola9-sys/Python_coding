f2b1fe1279765ae0b4fc61ead22dc11510f93da1
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-02 07:45:19 +0530

    added

--- Python_fundamentals_coding/files_ps/01_prblm1.py
+++ Python_fundamentals_coding/files_ps/01_prblm1.py
@@ -1,8 +1,7 @@
-from os import read
 f=open("peoms.txt")
 reading=f.read()
 if("twinkle " in reading):
-        print("twinkle is present in the txt")
+    print("twinkle is present in the txt")
 else:
     print("Twinkle is not present in the txt")
 f.close()
