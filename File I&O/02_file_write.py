498183c55191bb28d52599a2742c73b833709d4a
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-05 21:30:46 +0530

    files are added

--- File I&O/02_file_write.py
+++ File I&O/02_file_write.py
@@ -1,4 +1,5 @@
-text="hey there this is this and that is that luffy will become the king of the pirates and I will be master of python.. that is the objective of today itself "
-with open ("hello.txt","w") as f:
+text="There this is this and that is that luffy will become the king of the pirates and I will be master of python.. that is the objective of today itself "
+with open ("file.txt","w") as f:
     reading=f.write(text)
     print(reading) #prints the length of the hello.txt file..
+    f.close()
