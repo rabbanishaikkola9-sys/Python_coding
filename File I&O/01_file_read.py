0f70bd34021b5a9f73e1633c8b61476231a68685
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-05 20:37:28 +0530

    donebro done

--- File I&O/01_file_read.py
+++ File I&O/01_file_read.py
@@ -1,7 +1,4 @@
 f=open("hello.txt")
 reading=f.read()
-# print(reading)
+print(reading)
 f.close()
-text="hey there this is this and that is that luffy will become the king of the pirates and I will be master of python "
-with open ("hello.txt","w") as f:
-    reading=f.write(text)
---
+++ File I&O/02_file_write.py
@@ -0,0 +1,4 @@
+text="hey there this is this and that is that luffy will become the king of the pirates and I will be master of python.. that is the objective of today itself "
+with open ("hello.txt","w") as f:
+    reading=f.write(text)
+    print(reading) #prints the length of the hello.txt file..
