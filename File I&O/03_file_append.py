f6d19d825fce8636057beb070c127cdece6a335e
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-05 21:12:10 +0530

    added broo

---
+++ File I&O/03_file_append.py
@@ -0,0 +1,4 @@
+
+with open ("File I&O/hello.txt", "a") as f:
+    f.write("\nbyee and take care of your health because health is the wealth")
+    # print(reading) #prints the length of the hello.txt file.. 
