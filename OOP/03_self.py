18ca79dfc45fb099dfda68e3e56f7f6d216885e2
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-06 18:38:03 +0530

    Create 03_self.py

---
+++ OOP/03_self.py
@@ -0,0 +1,19 @@
+class Employee:
+    language="py" # This is the class attribute
+    Gender="male"
+    salary=120000
+    def greeteveryone(self):
+        print("Good morning everyone")
+
+    def getInfo(self):
+        print(f"The language is {self.language} and the salary is {self.salary}")
+rabbani=Employee()
+rabbani.name="Harry" # This is the instance /Object attribute
+print(rabbani.name,rabbani.language)
+rohan=Employee()
+rohan.name="Rohan"
+print(rohan.name)
+print(rohan.Gender,rohan.language)
+# here the name is obj attr nd the salary nd language are class attr as they directly belong to this class
+rabbani.getInfo()
+rabbani.greeteveryone()
