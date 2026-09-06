a9004757d8a7e136e7de7096e3a4fcb84b4f576c
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-06 09:49:30 +0530

    done bro done

--- File I&O/03_file_append.py
+++ File I&O/03_file_append.py
@@ -1,4 +1,5 @@
 
-with open ("File I&O/hello.txt", "a") as f:
+with open ("hello.txt", "a") as f:
     f.write("\nbyee and take care of your health because health is the wealth")
     # print(reading) #prints the length of the hello.txt file.. 
+# print("Hello world!!")
