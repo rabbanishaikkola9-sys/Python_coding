1ab2a4a950b7fdbd70b80b71a0405218af1884f8
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-06 20:02:44 +0530

    upto prblm4

---
+++ OOP/OOP_ps/prblm4.py
@@ -0,0 +1,17 @@
+class Calculator:
+    @staticmethod
+    def hello():
+        print("Hello world !!! ")
+    def __init__(self,n):
+        self.n=n
+    def square(self):
+        print(f"The square of the {self.n} is {self.n**2}")
+    def cube(self):
+        print(f"The cube of the {self.n} is {self.n**3}")
+    def squareroot(self):
+        print(f"The squareroot of the {self.n} is {self.n**(1/2)}")
+a=Calculator(25)
+a.hello()
+a.square()
+a.cube()
+a.squareroot()
