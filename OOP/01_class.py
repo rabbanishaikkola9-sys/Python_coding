5e6e16e928e7d8a4a953cf340a0ba10bb90d8bdd
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-06 18:15:14 +0530

    done the basics of basics

--- OOP/01_class.py
+++ OOP/01_class.py
@@ -1,7 +1,11 @@
 class Employee:
-    name="rabbani"
-    language="py"
+    language="py" # This is the class attribute
     Gender="male"
 harry=Employee()
-print(harry.name)
-print(harry.language)
+harry.name="Harry" # This is the instance /Object attribute
+print(harry.name,harry.language)
+rohan=Employee()
+rohan.name="Rohan"
+print(rohan.name)
+print(rohan.Gender,rohan.language)
+# here the name is obj attr nd the salary nd language are class attr as they directly belong to this class
---
+++ OOP/02_instance_vs_cls_attributes.py
@@ -0,0 +1,7 @@
+class Employee:
+    name="rabbani"
+    language="py" # This is the class attribute
+    Gender="male"
+harry=Employee()
+harry.language="JS" # This is the instance /Object attribute
+print(harry.name,harry.language)
