2bdf5155ee324863aa23a83bb3fede3cf00f693a
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-07 21:46:43 +0530

    Update 01_class.py

---
+++ OOP-2/01_class.py
@@ -0,0 +1,9 @@
+class employee:
+    name="rabbani" # class attribute
+    age=18 # class attribute
+    lang="Python" # class attribute
+    sex="Male" # class attribute
+obj1=employee()
+print(obj1.name,obj1.age,obj1.lang,obj1.sex)
+obj2=employee()
+print(obj2.name,obj2.age,obj2.lang,obj2.sex)
