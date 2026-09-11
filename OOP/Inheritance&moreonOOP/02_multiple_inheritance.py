80e47397af7db04946268ef5bdd1bcf3c37a450d
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-11 07:17:20 +0530

    done bro done

--- OOP/Inheritance&moreonOOP/02_multiple_inheritance.py
+++ OOP/Inheritance&moreonOOP/02_multiple_inheritance.py
@@ -1,12 +1,17 @@
 class employee:
     company="ITC"
+    name="Rabbani"
     def show(self):
         print(f"The name is {self.name } and the salary is {self.salary}")
 # class programmer:
-class programmer(employee):
+class coder:
+    language="Python"
+    def printlanguages(self):
+        print("Out of all the languages here is your language {self.language}")
+class programmer(employee,coder):
     company="ITCinfotech"
     def showlang(self):
-        print(f"The name is {self.name } and he is good at {self.lang}")
+        print(f"The name is {self.company} and he is good at {self.language}")
     @staticmethod
     def greet():
         print("hey good morning")
@@ -15,3 +20,5 @@ b=programmer()
 programmer.greet()
 print(a.company)
 print(b.company)
+b.showlang()
+b.printlanguages()
