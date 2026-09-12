a191e0e3ab753913836844ba3b1240a1292b3acf
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-09-12 19:11:15 +0530

    done bro done

--- OOP/02_instance_vs_cls_attributes.py
+++ OOP/02_instance_vs_cls_attributes.py
@@ -1,7 +1,12 @@
 class Employee:
-    name="rabbani"
+    # name="rabbani"
     language="py" # This is the class attribute
     Gender="male"
 harry=Employee()
+harry.name="harry"
 harry.language="JS" # This is the instance /Object attribute
 print(harry.name,harry.language)
+rohan=Employee()
+rohan.name="rohan"
+rohan.language="Kotlin"
+print(rohan.name,rohan.language)
